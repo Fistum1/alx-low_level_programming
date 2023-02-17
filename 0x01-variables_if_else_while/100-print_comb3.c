91c6b3f859dd4c38d7f2d7580bdd5c1a9044d633
Fistum1 <fistumitiku1@gmail.com>
2023-02-17 03:15:22 -0800

    test c variables

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,33 +1,28 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - a simple program that outputs 0-9 separated by commas
  *
- * Return: Always 0 (Succes)
+ * Return: 0 on success
  */
-
 int main(void)
 {
-	int n;
-	int i = 0;
+	int i;
+	int j;
 
-	for (n = 0 ; n < 100 ; n++)
+	for (i = 48; i < 57; i++)
 	{
-		putchar(n / 10 + '0');
-		putchar(i + '0');
-		if (n < 99)
+		for (j = i + 1; j < 58; j++)
 		{
-			putchar(',');
-			putchar(32);
-		}
-		i++;
-		if (i > 9)
-		{
-			i = 0;
+			putchar(i);
+			putchar(j);
+			if (i != 56 || j != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
 	return (0);
 }
-
-
