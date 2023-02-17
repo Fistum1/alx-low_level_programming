c2b5db9455600c951d9f5fe85af2d3b27b01f584
Fistum1 <fistumitiku1@gmail.com>
2023-02-17 03:12:28 -0800

    test c variables

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,21 @@
 int main(void)
 {
 	int n;
+	int i = 0;
 
-	for (n = 0 ; n < 10 ; n++)
+	for (n = 0 ; n < 100 ; n++)
 	{
-		putchar(n + '0');
-		if (n < 9)
+		putchar(n / 10 + '0');
+		putchar(i + '0');
+		if (n < 99)
 		{
 			putchar(',');
 			putchar(32);
+		}
+		i++;
+		if (i > 9)
+		{
+			i = 0;
 		}
 	}
 	putchar('\n');
