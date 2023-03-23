c6f4ef2a673b288c936b7b143fa6005aa4fa6e17
Fistum1 <122812540+Fistum1@users.noreply.github.com>
2023-03-23 19:03:25 +0300

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,8 +2,6 @@
 
 /**
  * reverse_array -  function that reverses the content of an array of integers.
- * @a: puntero del array.
- * @n: numero de elementos del array.
  */
 
 void reverse_array(int *a, int n)
