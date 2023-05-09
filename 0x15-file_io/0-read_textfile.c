991f064ed5b507d45ef22c20e562d315ae942714
Fistum1 <fistumitiku@gmail.com>
2023-05-09 19:21:00 +0300

    test i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,37 +3,37 @@
 
 /**
  *read_textfile - Reads and prints it to the POSIX standard output.
- *@filename1: File name.
- *@letters1: Number of letters to print.
+ *@filename: File name.
+ *@letters: Number of letters to print.
  *Description - read_textfile: Function that reads a text file and print.
  *Return: Null if it failes.
  */
 
-ssize_t read_textfile(const char *filename1, size_t letters1)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
+	ssize_t ox, rx, wx;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters1);
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 
-	o = open(filename1, O_RDONLY);
-	r = read(o, buffer, letters1);
-	w = write(STDOUT_FILENO, buffer, r);
+	ox = open(filename, O_RDONLY);
+	rx = read(ox, buffer, letters);
+	wx = write(STDOUT_FILENO, buffer, rx);
 
-	if (o == -1 || r == -1 || w == -1 || w != r)
+	if (ox == -1 || rx == -1 || wx == -1 || wx != rx)
 	{
 		free(buffer);
 		return (0);
 	}
 
 	free(buffer);
-	close(o);
+	close(ox);
 
-	return (w);
+	return (wx);
 }
 
